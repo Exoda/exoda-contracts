d997d40ee8b2aad705120f4e7cd75cdda8f7eebf
Ing. Michael Goldfinger <106750422+migoldfinger@users.noreply.github.com>
2022-07-29 15:04:06 +0200

    feat: optimize inheritance

--- contracts/token/ERC20/extensions/ERC20Burnable.sol
+++ contracts/token/ERC20/extensions/ERC20Burnable.sol
@@ -10,7 +10,7 @@ import "../../../utils/Context.sol";
 * tokens and those that they have an allowance for, in a way that can be
 * recognized off-chain (via event analysis).
 */
-contract ERC20Burnable is IERC20Burnable, Context, ERC20
+contract ERC20Burnable is Context, ERC20, IERC20Burnable
 {
 	/**
 	* @notice Sets the values for {name} and {symbol}.
