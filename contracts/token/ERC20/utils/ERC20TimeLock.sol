6bfd1470a13fe123dfe42ad30de61ca086dacc65
Ing. Michael Goldfinger <106750422+migoldfinger@users.noreply.github.com>
2023-04-21 16:43:06 +0200

    fix: lint (#43)

--- contracts/token/ERC20/utils/ERC20TimeLock.sol
+++ contracts/token/ERC20/utils/ERC20TimeLock.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import { IERC20Metadata, ERC20 } from "../ERC20.sol";
 import { IERC20 } from "../../../interfaces/token/ERC20/IERC20.sol";
 
+// solhint-disable not-rely-on-time
+
 interface ITokenLock is IERC20Metadata
 {
 	/// Withdraw amount exceeds sender's balance of the locked token
@@ -16,6 +18,9 @@ interface ITokenLock is IERC20Metadata
 	error TransferFailed();
 	/// ERC-20 function is not supported
 	error NotSupported();
+
+	function deposit(uint256 amount) external;
+	function withdraw(uint256 amount) external;
 
 	function token() external view returns(IERC20Metadata);
 	function depositDeadline() external view returns(uint256);
@@ -43,7 +48,7 @@ contract ERC20TimeLock is ERC20, ITokenLock {
 
 	/// @dev Deposit tokens to be locked until the end of the locking period
 	/// @param amount The amount of tokens to deposit
-	function deposit(uint256 amount) external {
+	function deposit(uint256 amount) external override {
 		if (block.timestamp > _depositDeadline) {
 			revert DepositPeriodOver();
 		}
@@ -60,7 +65,7 @@ contract ERC20TimeLock is ERC20, ITokenLock {
 
 	/// @dev Withdraw tokens after the end of the locking period or during the deposit period
 	/// @param amount The amount of tokens to withdraw
-	function withdraw(uint256 amount) external {
+	function withdraw(uint256 amount) external override {
 		if (
 			block.timestamp > _depositDeadline &&
 			block.timestamp < _depositDeadline + _lockDuration
@@ -79,6 +84,18 @@ contract ERC20TimeLock is ERC20, ITokenLock {
 		}
 
 		emit Transfer(address(this), _msgSender(), amount);
+	}
+
+	function token() external view override returns (IERC20Metadata) {
+		return _token;
+	}
+
+	function depositDeadline() external view override returns (uint256) {
+		return _depositDeadline;
+	}
+
+	function lockDuration() external view override returns (uint256) {
+		return _lockDuration;
 	}
 
 	/// @dev Lock claim tokens are non-transferrable: ERC-20 transfer is not supported
@@ -112,17 +129,5 @@ contract ERC20TimeLock is ERC20, ITokenLock {
 		uint256
 	) public pure override(ERC20, IERC20) returns (bool) {
 		revert NotSupported();
-	}
-
-	function token() external view override returns (IERC20Metadata) {
-		return _token;
-	}
-
-	function depositDeadline() external view override returns (uint256) {
-		return _depositDeadline;
-	}
-
-	function lockDuration() external view override returns (uint256) {
-		return _lockDuration;
 	}
 }
